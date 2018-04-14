1ac3f6b3b64760fc6a2739991bb0a24dba91f4ac
Ильфар Хузин <ilfarhuzin@gmail.com>
2018-04-14 14:36:43 +0300

    Alpha 0.1.2.1
    Удалён класс для сохранения/загрузки состояния программы из внешнего файлы. Все методы этого класса перенесены в класс Main.

--- src/me/ilfar/FileManagement.java
+++
@@ -1,6 +0,0 @@
-package me.ilfar;
-
-//методы работы с файлами
-public class FileManagement {
-
-}
--- src/me/ilfar/Main.java
+++ src/me/ilfar/Main.java
@@ -2,6 +2,13 @@ package me.ilfar;
 
 public class Main {
 
+    public void SaveAll() {
+        //метод сохранения состояния программы на внешнем файле
+    }
+    public void OpenSaved() {
+        //метод открытия сохранённого состояния программы из внешнего файла
+    }
+
     public static void main(String[] args) {
 	// write your code here
     }
